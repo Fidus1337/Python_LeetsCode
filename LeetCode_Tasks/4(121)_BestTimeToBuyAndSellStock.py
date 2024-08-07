07d74043d0e9041be90a04f0ca139a5fa30af7ed
Fidus1337 <121771130+Fidus1337@users.noreply.github.com>
2024-08-07 08:59:49 +0200

    New and the best solution for 121 task (Best time to buy and sell stock)

--- LeetCode_Tasks/4(121)_BestTimeToBuyAndSellStock.py
+++ LeetCode_Tasks/4(121)_BestTimeToBuyAndSellStock.py
@@ -7,20 +7,25 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+        """This solution takes a lot of time for searching result O(n^2)"""
         max_profit = 0
         for first_day_index, first_day_price in enumerate(prices):
             for second_day_index, second_day_price in enumerate(prices[first_day_index + 1:]):
-                if second_day_index <= first_day_index:
-                    continue
-                elif second_day_price - first_day_price > max_profit:
+                if second_day_price - first_day_price > max_profit:
                     max_profit = second_day_price - first_day_price
         return max_profit
 
+    def maxProfit2(self, prices):
+        """The best solution with spending time O(n)"""
+        profit = 0
+        min_price = prices[0]
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            profit = max(profit, price - min_price)
+        return profit
 
-prices = [7, 1, 5, 3, 6, 4]
+
+prices = [7, 5, 3, 6, 4, 1]
 sol = Solution()
 print(sol.maxProfit(prices))
